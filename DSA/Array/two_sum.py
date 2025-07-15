52dde31b71bbc716cecd548cd85ff3d693064e2d
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-07-15 11:26:01 +0600

    logest common prefix

--- DSA/Array/two_sum.py
+++ DSA/Array/two_sum.py
@@ -1,4 +1,8 @@
 def two_sum(nums , target):
+    #Pattern : - Hashmap
+    #Using a dictionary to store the indices of the numbers
+    #This allows us to find the complement in O(1) time
+    
     seen = {}
     for i, num in enumerate(nums):
         complement = target - num
