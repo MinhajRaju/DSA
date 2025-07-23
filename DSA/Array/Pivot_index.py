9c6f6a169814006ff8b6a1ee7d5c29a5edeece3d
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-07-23 15:57:36 +0600

    Pivot index

---
+++ DSA/Array/Pivot_index.py
@@ -0,0 +1,15 @@
+
+def pivotIndex( nums):
+
+    total = sum(nums[:len(nums)])
+    left_sum = 0
+
+    for i in range(len(nums)):
+        right = total - left_sum - nums[i]
+        print(right , total , left_sum)
+        if right == left_sum: 
+            print("Found pivot at index:", i)           
+            return i
+        left_sum += nums[i]
+    return -1
+pivotIndex([1, 7, 3, 6, 5, 6])   
